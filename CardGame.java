85a28d03df07d2940d22fed22b726fdc9d3fb4d1
Ryan Mailhiot <100972421+LowkeyMadLad@users.noreply.github.com>
2023-03-10 23:41:43 -0700

    Add files via upload

---
+++ CardGame.java
@@ -0,0 +1,37 @@
+public class CardGame {
+    private Player player1;
+    private Player player2;
+
+
+
+    public CardGame(Player p1, Player p2){
+
+    }
+
+    // Making this static otherwise having the player arguments makes no sense
+    public static void updatePlayerStats(Player winner, Player loser){
+        
+    }
+
+    public String runGame(){
+
+        return "hi";
+    }
+
+    public void readGame(String seed){
+
+    }
+
+    public void saveGameToDB(){
+
+    }
+
+    public Player getPlayer1() {
+        return this.player1;
+    }
+
+    public Player getPlayer2() {
+        return this.player2;
+    }
+
+}

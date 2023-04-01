514d51d5853a899d131ee86e73f999f335bd2519
LowkeyMadLad <ryan.mailhiot@ucalgary.ca>
2023-04-01 01:24:59 -0600

    Functional cards page

--- WebApp/html/js/app.js
+++ WebApp/html/js/app.js
@@ -12,18 +12,43 @@ draggables.forEach(draggable => {
     })
 })
 
+// This section is dealing specifically with the cards on the right hand side
 selectedCardSub.forEach(selectCard => {
-    selectCard.addEventListener('dragover', e => {
+    selectCard.addEventListener("dragover", e => {
+        e.preventDefault()
+    })
+
+    selectCard.addEventListener("drop", e => {
         e.preventDefault()
         const draggable = document.querySelector('.dragging')
-        console.log(selectCard.childNodes[1])
-        var dragID = draggable.id
-        if(selectCard.childNodes.length == 1){
+        if(selectCard.childNodes.length == 1){ // When no card is in there
             selectCard.appendChild(draggable)
+            if (selectCard.id === "body-card-slot") {
+                draggable.classList.add('body-card-slot')
+            } else {
+                draggable.classList.add('hand-card-slot')
+            }
         }
-        else if(selectCard.childNodes.length == 2 && selectCard.childNodes[1].id != dragID){
+        else{ // When a card is in there
+            if (selectCard.id === "body-card-slot") {
+                const replaceable = document.querySelector('.body-card-slot') // Gets the item currently in the body slot
+                var sendToSlot = replaceable.id + "-holder";
+                const replacableSlot = document.getElementById(sendToSlot)
+                replaceable.classList.remove('body-card-slot')
+                replacableSlot.appendChild(replaceable)
+                selectCard.appendChild(draggable)
+                draggable.classList.add('body-card-slot')
+            } else {
+                const replaceable = document.querySelector('.hand-card-slot') // Gets the item currently in the body slot
+                var sendToSlot = replaceable.id + "-holder";
+                const replacableSlot = document.getElementById(sendToSlot)
+                replaceable.classList.remove('hand-card-slot')
+                replacableSlot.appendChild(replaceable)
+                selectCard.appendChild(draggable)
+                draggable.classList.add('hand-card-slot')
+            }
+        }
 
-        }
         //var elementID = document.getElementById(selectCard.childNodes[0].id)
         // This doesnt quite have all the functionality. Basically I am trying to find a way to grab the
         // ID of the item currently in the node and exchange it with the one that is being dragged

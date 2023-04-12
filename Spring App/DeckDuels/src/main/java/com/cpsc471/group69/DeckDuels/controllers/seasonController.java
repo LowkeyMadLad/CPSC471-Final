938c2675db5b0a15857ef87e9fbd05dcec51d433
LowkeyMadLad <ryan.mailhiot@ucalgary.ca>
2023-04-12 13:20:05 -0600

    formatting for season

--- Spring App/DeckDuels/src/main/java/com/cpsc471/group69/DeckDuels/controllers/seasonController.java
+++ Spring App/DeckDuels/src/main/java/com/cpsc471/group69/DeckDuels/controllers/seasonController.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 
 import org.springframework.stereotype.Controller;
 // import org.springframework.ui.Model;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import com.cpsc471.group69.DeckDuels.game.CardDatabase;
@@ -15,36 +16,9 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Controller
 public class seasonController {
-    CardDatabase db = CardDatabase.getDB();
 
-    @GetMapping("/season/getallseasonpeaks/{username}")
-    public ArrayList<String> getAllSeasonPeaks(@PathVariable(value = "username") String username){
-        ArrayList<String> list = new ArrayList<String>();
-        ArrayList<SeasonPeak> seasonList = new ArrayList<SeasonPeak>();
-
-        try {
-            seasonList = db.getSeasonPeaks(username);
-        } catch (DBConnectException | SQLException e) {
-            e.printStackTrace();
-        }
-
-        if(seasonList.isEmpty()){
-            // something to display nothing?
-        }
-
-        String jsonStr;
-        ObjectMapper mapper = new ObjectMapper();
-        for(SeasonPeak sp : seasonList){
-            try {
-                jsonStr = mapper.writeValueAsString(sp);
-                list.add(jsonStr);
-            } catch (JsonProcessingException e) {
-                e.printStackTrace();
-            }
-        }
-
-        // returns array list of json strings
-        return list;
+    @GetMapping("/season")
+    public String season(Model model){
+        return "season";
     }
-
 }
---
+++ Spring App/DeckDuels/src/main/java/com/cpsc471/group69/DeckDuels/sqlFunctions/seasonDBController.java
@@ -0,0 +1,50 @@
+package com.cpsc471.group69.DeckDuels.sqlFunctions;
+
+import com.cpsc471.group69.DeckDuels.game.CardDatabase;
+import com.cpsc471.group69.DeckDuels.game.DBConnectException;
+import com.cpsc471.group69.DeckDuels.game.SeasonPeak;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+@RestController
+public class seasonDBController {
+
+    CardDatabase db = CardDatabase.getDB();
+
+    @GetMapping("/season/getallseasonpeaks/{username}")
+    public ArrayList<String> getAllSeasonPeaks(@PathVariable(value = "username") String username){
+        ArrayList<String> list = new ArrayList<String>();
+        ArrayList<SeasonPeak> seasonList = new ArrayList<SeasonPeak>();
+
+        try {
+            seasonList = db.getSeasonPeaks(username);
+        } catch (DBConnectException | SQLException e) {
+            e.printStackTrace();
+        }
+
+        if(seasonList.isEmpty()){
+            // something to display nothing?
+        }
+
+        String jsonStr;
+        ObjectMapper mapper = new ObjectMapper();
+        for(SeasonPeak sp : seasonList){
+            try {
+                jsonStr = mapper.writeValueAsString(sp);
+                list.add(jsonStr);
+            } catch (JsonProcessingException e) {
+                e.printStackTrace();
+            }
+        }
+
+        // returns array list of json strings
+        return list;
+    }
+
+}
